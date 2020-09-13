fd2a7235cdf0d8a922e30947340ca918ba3aa086
Rolando Arevalo <rolando452000@gmail.com>
2020-09-13 10:51:52 -0500

    se agrega tarea 3

---
+++ tarea3/Fibo.java
@@ -0,0 +1,20 @@
+class Fibo {
+
+    public static void main(final String args[]) {
+
+        int valor1 = 0;
+        int valor2 = 1;
+        int i = 0;
+
+        System.out.println(valor1);
+        System.out.println(valor2);
+
+        while (i < 10) {
+            final int valor3 = valor1 + valor2;
+    System.out.println(valor3);
+    i++;
+    valor1 = valor2;
+    valor2 = valor3;
+    }
+    }
+}
